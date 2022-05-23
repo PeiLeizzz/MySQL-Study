b29c8ddb2f808b37b105b89ffc69e2536a70c91f
PeiLeizzz <714838284@qq.com>
2022-05-23 22:51:49 +0800

    UPDATE: Redis 主从复制; 哨兵模式; 集群; 应用问题; Redis 6 新特性

---
+++ Redis6基础/myredis/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+redis-server redis6379.conf
+redis-server redis6380.conf
+redis-server redis6381.conf
+redis-server redis6389.conf
+redis-server redis6390.conf
+redis-server redis6391.conf
